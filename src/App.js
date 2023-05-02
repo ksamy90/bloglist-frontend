bf87fabab2dd12afae3b22280f9e1edb4647c7cb
samson <samson.omondi1@ibm.com>
2023-05-02 14:38:26 +0300

    sort array of components

--- src/App.js
+++ src/App.js
@@ -97,9 +97,11 @@ const App = () => {
           <Togglable buttonLabel="new blog" ref={blogFormRef}>
             <BlogForm createBlog={addBlog} />
           </Togglable>
-          {blogs.map((blog) => (
-            <Blog key={blog.id} blog={blog} updateBlog={updateLike} />
-          ))}
+          {blogs
+            .sort((a, b) => b.likes - a.likes)
+            .map((blog) => {
+              return <Blog key={blog.id} blog={blog} updateBlog={updateLike} />;
+            })}
         </div>
       )}
     </div>
